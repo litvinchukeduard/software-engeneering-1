618a472215184745566c2b22ba181741f38602e2
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2024-11-23 16:49:41 +0200

    Check password other regexp

--- 2/check_password.py
+++ 2/check_password.py
@@ -1,10 +1,10 @@
 import re
 
 def check_password(password: str) -> bool:
-    pattern = r"(?=.*\d)(?=.*[A-Z])(?=.*[+=-\\])[\w\d+=-\\]{8}"
-    []
+    pattern = r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[+=-\\])[\w\d]{6}"
     return bool(re.fullmatch(pattern, password))
 
+# regex=r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\wA-Za-z]{6,}"
 
 assert check_password('password') == False
 assert check_password('aaaaaA_4') == True
