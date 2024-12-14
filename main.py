bc5cb0ef1387c0aea686662f9303c4d9c02d9d5b
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2024-12-14 16:56:32 +0200

    UserDict example

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-# from module.function import hello
-from module_two.functions import world
+from collections import UserDict
+# # from module.function import hello
+# from module_two.functions import world
 
-world()
+# world()
+
+class MyDict(UserDict):
+    def do_something(self):
+        self.get('key1')
+
+my_regular_dict = {'key1': 1, 'key2': 2}
+my_class_dict = MyDict({'key1': 1, 'key2': 2})
+print(my_class_dict)
+
+print(my_regular_dict['key1'])
+print(my_class_dict['key1'])
+
+my_class_dict.
